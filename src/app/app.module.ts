0a3ebc8f1a9ecd6166c40ec5b15ed5feb0b8aa1a
Valen Cheung <xvalen214x@gmail.com>
2019-12-30 19:49:18 +0800

    adding kana origin

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,5 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
 
@@ -20,6 +21,7 @@ import { KanaTableComponent } from './kana-table/kana-table.component';
    ],
    imports: [
       BrowserModule,
+      BrowserAnimationsModule,
       AppRoutingModule,
       HttpClientModule
    ],
